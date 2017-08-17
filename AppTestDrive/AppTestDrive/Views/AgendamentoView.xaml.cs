cb86b37f85cb918fabb9c80659b5cf50f66696a7
Ricardo Takuno <takunoricardo@gmail.com>
2017-08-16 22:28:28 -0300

    denvolvendo aplicativo teste drive

---
+++ AppTestDrive/AppTestDrive/Views/AgendamentoView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTestDrive.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AgendamentoView : ContentPage
+	{
+		public AgendamentoView (Veiculo veiculo)
+		{
+			InitializeComponent ();
+            this.Title = veiculo.Nome;
+        }
+	}
+}
--- AppTestDrive/AppTestDrive/Views/DetalheView.xaml.cs
+++ AppTestDrive/AppTestDrive/Views/DetalheView.xaml.cs
@@ -13,7 +13,89 @@ namespace AppTestDrive.Views
 	public partial class DetalheView : ContentPage
 	{
         public Veiculo Veiculo { get; set; }
-		public DetalheView (Veiculo veiculo)
+        private const int VALOR_FREIO_ABS = 800;
+        private const int VALOR_AR_CONDICIONADO = 1000;
+        private const int VALOR_MP3_PLAYER = 500;
+
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R${0}", VALOR_FREIO_ABS);
+            }
+        }
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R${0}", VALOR_AR_CONDICIONADO);
+            }
+        }
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R${0}", VALOR_MP3_PLAYER);
+            }
+        }
+        bool temFreioABS;
+        public bool TemFreioABS
+        {
+            get
+            {
+                return temFreioABS;
+            }
+            set
+            {
+                temFreioABS = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PrecoTotalFormatado));
+            }
+        }
+        bool temArCondicionado;
+        public bool TemArCondicionado
+        {
+            get
+            {
+                return temArCondicionado;
+            }
+            set
+            {
+                temArCondicionado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PrecoTotalFormatado));
+            }
+        }
+        bool temMP3Player;
+        public bool TemMP3Player
+        {
+            get
+            {
+                return temMP3Player;
+            }
+            set
+            {
+                temMP3Player = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(PrecoTotalFormatado));
+            }
+        }
+
+        public decimal PrecoTotal
+        {
+            get
+            {
+                return Veiculo.Preco + (TemFreioABS ? VALOR_FREIO_ABS : 0) + (TemArCondicionado ? VALOR_AR_CONDICIONADO : 0) + (TemMP3Player ? VALOR_MP3_PLAYER : 0);
+            }
+        }
+        public string PrecoTotalFormatado
+        {
+            get
+            {
+                return string.Format("Total: R$ {0}", PrecoTotal);
+            }
+        }
+        public DetalheView (Veiculo veiculo)
 		{
             this.Veiculo = veiculo;
 			InitializeComponent ();
@@ -21,7 +103,7 @@ namespace AppTestDrive.Views
 		}
         private void botaoProximo_Clicked(object sender, EventArgs e)
         {
-
-        }
+            Navigation.PushAsync(new AgendamentoView(this.Veiculo));
+        } 
 	}
 }
